730b1cf2cdd3a9e6ce5cc80c58c6cac1ed4055c9
luowailin <blazer402@126.com>
2020-03-13 17:22:18 +0800

    add

---
+++ CommonClass-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CommonClass-cpp
+//
+//  Created by luowailin on 2020/3/12.
+//  Copyright © 2020 luowailin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
