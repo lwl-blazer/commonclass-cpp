984357a79d9d08f6314e142cd16b29b1f3763460
blazer402@126.com <blazer402>
2020-04-15 22:26:30 +0800

    model route

--- CommonClass-cpp/route_model.hpp
+++ CommonClass-cpp/route_model.hpp
@@ -10,5 +10,24 @@
 #define route_model_hpp
 
 #include <stdio.h>
+#include <limits>
+#include <cmath>
+#include <unordered_map>
+#include <iostream>
+#include "model.hpp"
+
+class Route_Model : public Model{
+public:
+    class Node : public Model::Node{
+    public:
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        
+    };
+};
+
+
+
 
 #endif /* route_model_hpp */
